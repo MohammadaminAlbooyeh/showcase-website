995139b74c03e2ca8662db18f2df3d10bb0fdc67
Amin Albooyeh <amin.albooyeh@gmail.com>
2025-07-14 21:41:21 +0200

    solving some conflict

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def root_view(request):
+    return JsonResponse({"message": "Django backend is running."})
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("portfolio.urls")),
+    path('', root_view),
+    path('admin/', admin.site.urls),
+    path('api/', include('portfolio.urls')),
 ]
