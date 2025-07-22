124dd1bf93fc94bc43fd95db2e3f9447834e11f0
Amin <amin.albooyeh@gmail.com>
2025-07-22 14:13:01 +0200

    updating home page to solve some conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,20 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<AboutMe />} />
-        <Route path="/about" element={<AboutMe />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/contact/links" element={<ContactLinks />} />
-      </Routes>
+      <div className="App">
+        <NavBar />
+        <main className="content">
+          <Routes>
+            <Route path="/" element={<AboutMe />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/contact/links" element={<ContactLinks />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
