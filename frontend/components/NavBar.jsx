2c7728ce9bf8a0dc526d02e9563d5c8203049964
Amin Albooyeh <amin.albooyeh@gmail.com>
2025-08-19 20:35:58 +0200

    update the top bottons

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import './NavBar.css';
+
 
 export default function NavBar() {
   const router = useRouter();
---
+++ pages/_app.jsx
@@ -0,0 +1,17 @@
+import '../components/FixedBottomBar.css';
+import '../App.css';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import FixedBottomBar from '../components/FixedBottomBar';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <NavBar />
+      <main className="content">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
+}
